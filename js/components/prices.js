8275085e25709ad0fe0017378ac219f55a9154cd
Lean <leian1306@gmail.com>
2023-12-14 11:53:37 +0100

    Adapt service pages and add links in prices

--- js/components/prices.js
+++ js/components/prices.js
@@ -5,16 +5,16 @@ class Prices extends HTMLElement {
 
     connectedCallback() {
         this.innerHTML = `
-        <div class="offer">
+        <a href="./contact.html" class="offer">
             <ul class="price">
                 <li class="header"><h6>The Spark</h6></li>
                 <li class="grey cost"><h1>300€</h1></li>
                 <li><strong>1 hour of brainstorming</strong> solutions with you</li>
                 <li><strong>2 hours of 1:1 coaching</strong> for any team member</li>
             </ul>
-        </div>
+        </a>
 
-        <div class="offer">
+        <a href="./contact.html" class="offer">
             <ul class="price highlighted">
                 <li class="header"><h6>The Bright Spot</h6></li>
                 <li class="grey cost"><h1>1000€</h1></li>
@@ -22,16 +22,16 @@ class Prices extends HTMLElement {
                 <li><strong>5 hours of 1:1 coaching</strong> for any team member</li>
                 <li><strong>Summary document</strong> of my findings, feedback, and potential improvements</li>
             </ul>
-        </div>
+        </a>
 
-        <div class="offer">
+        <a href="./contact.html" class="offer">
             <ul class="price">
                 <li class="header"><h6>The Shine</h6></li>
                 <li class="grey cost"><h2>On demand</h2></li>
-                <li>Do you have something else in mind?</li>
-                <li>Let's discuss how I can help you</li>
+                <li>Something else in mind?</li>
+                <li>Let's talk about your ideas</li>
             </ul>
-        </div>
+        </a>
         `;
     }
 }
