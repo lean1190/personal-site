16f7d809379c8d2966248493bdb9b61c7566cae8
Lean <leian1306@gmail.com>
2024-04-25 17:33:20 +0200

    layout: simplify and clarify title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import type { Metadata } from 'next';
 import { roboto } from './fonts/fonts';
 
 export const metadata: Metadata = {
-    title: 'Mentor and Coach - Lean Vilas',
+    title: 'Mentor and Coach for Software Engineers',
     description: 'Become a CONFIDENT Software Engineer, learn faster, upskill your coding, and get the job you are looking for.',
     icons: { icon: '/favicon.ico' },
     metadataBase: new URL(process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : `http://localhost:${process.env.PORT || 3000}`),
