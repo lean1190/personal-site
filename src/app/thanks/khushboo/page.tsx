61d97a49abbbe5ab779614ed58ad41ae31d9103e
Lean <leian1306@gmail.com>
2024-05-26 13:48:34 +0200

    thanks: add card for Khushboo

---
+++ src/app/thanks/khushboo/page.tsx
@@ -0,0 +1,12 @@
+import Card from '../components/card';
+
+export default function Khushboo() {
+    return <Card
+        hi="Hi Khushboo!"
+        backgroundClasses="bg-gradient-to-b from-pink-100 to-pink-300"
+        paragraphs={[
+            'Thanks for trusting me with your job hunting preparation. I hope you got some good insights :)',
+            'Looking forward to the next sessions!'
+        ]}
+    />;
+}
