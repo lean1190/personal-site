4263e4f8e1b05475f7fdd4ce49817be0d441695e
Lean <leian1306@gmail.com>
2024-06-06 15:36:11 +0200

    links: replace calendly with tidycal

--- src/app/constants/links.ts
+++ src/app/constants/links.ts
@@ -1,6 +1,6 @@
 import { isDevEnvironment } from '@/lib/environments/is-dev';
 
-export const contactLink = 'https://calendly.com/leanvilas/confidence-session';
+export const contactLink = 'https://tidycal.com/leanvilas/confidence-session';
 export const adplistLink = 'https://adplist.org/mentors/leandro-nicolas-vilas';
 export const linkedinLink = 'https://www.linkedin.com/in/leanvilas/';
 export const homeLink = isDevEnvironment() ? `http://localhost:${process.env.PORT || 3000}` : 'https://leanvilas.com';
