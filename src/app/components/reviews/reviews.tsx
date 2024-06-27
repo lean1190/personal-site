39347a552efa8122a5c4a721b880e80070756a4a
Lean <leian1306@gmail.com>
2024-06-27 14:48:17 +0200

    reviews: add HCS logo

--- src/app/components/reviews/reviews.tsx
+++ src/app/components/reviews/reviews.tsx
@@ -3,7 +3,7 @@ import clsx from 'clsx';
 import ky from 'ky';
 import Image from 'next/image';
 
-import { adplistLink } from '@/app/constants/links';
+import { adplistLink, hcsLink } from '@/app/constants/links';
 import { shuffle } from '@/lib/arrays';
 
 import SectionHeader from '../ui/section-header';
@@ -41,28 +41,40 @@ export default async function Reviews() {
                     <ul className={marqueeListStyles} aria-hidden="true">{reviews.map((r) => <ReviewItem key={r.id} review={r} />)}</ul>
                 </div>
             </section>
-            <section className="mx-auto flex w-fit flex-col items-center gap-12 sm:flex-row">
+            <section className="mx-auto flex w-fit flex-col items-center gap-20 sm:flex-row">
                 <div className="flex flex-col items-center justify-between gap-12 sm:flex-row">
                     <div className="text-center">
-                        <p className="text-7xl font-bold">{statistics.sessions_completed + directMentees}+</p>
-                        <p className="text-2xl font-light">Mentees 💜</p>
+                        <p className="text-8xl font-bold sm:text-7xl">{statistics.sessions_completed + directMentees}+</p>
+                        <p className="text-3xl font-light sm:text-2xl">Mentees 💜</p>
                     </div>
                     <div className="text-center">
-                        <p className="text-7xl font-bold">{statistics.average_attendance}%</p>
-                        <p className="text-2xl font-light">Attendance rate</p>
+                        <p className="text-8xl font-bold sm:text-7xl">{statistics.average_attendance}%</p>
+                        <p className="text-3xl font-light sm:text-2xl">Attendance rate</p>
                     </div>
                 </div>
-                <Link
-                    href={adplistLink}
-                    target="_blank"
-                    rel="nofollow"
-                    className="w-full text-slate-800"
-                >
-                    <div className="flex w-full items-center justify-center gap-5">
-                        <Image src="/toprated.png" alt="ADPList top rated" width={100} height={115} />
+
+                <Image className="hidden sm:block" src="/toprated.png" alt="ADPList top rated" width={100} height={115} />
+
+                <div className="flex flex-col gap-12 sm:flex-row">
+                    <Link
+                        href={adplistLink}
+                        target="_blank"
+                        rel="nofollow"
+                        className="w-full text-slate-800"
+                    >
                         <Image src="/adplist_logo.svg" alt="ADPList logo" width={200} height={51} />
-                    </div>
-                </Link>
+                    </Link>
+                    <Link
+                        href={hcsLink}
+                        target="_blank"
+                        rel="nofollow"
+                        className="w-full text-slate-800"
+                    >
+                        <div>
+                            <Image src="/hcs_logo.png" alt="Hamburg Coding School logo" width={200} height={94} />
+                        </div>
+                    </Link>
+                </div>
             </section>
         </article>
     );
--- src/app/constants/links.ts
+++ src/app/constants/links.ts
@@ -3,4 +3,5 @@ import { isDevEnvironment } from '@/lib/environments/is-dev';
 export const contactLink = 'https://tidycal.com/leanvilas/confidence-session';
 export const adplistLink = 'https://adplist.org/mentors/leandro-nicolas-vilas';
 export const linkedinLink = 'https://www.linkedin.com/in/leanvilas/';
+export const hcsLink = 'https://hamburgcodingschool.com/en/team/lean/';
 export const homeLink = isDevEnvironment() ? `http://localhost:${process.env.PORT || 3000}` : 'https://leanvilas.com';
