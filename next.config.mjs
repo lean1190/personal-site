6578bfa6282c87964318b93d4880c1fc50b32049
Lean <leian1306@gmail.com>
2024-07-17 15:40:10 +0200

    portfolio: add some more images and elements
    This needs a redesign

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,18 @@ const nextConfig = {
             {
                 protocol: 'https',
                 hostname: 'adplist-bucket.s3.amazonaws.com'
+            },
+            {
+                protocol: 'https',
+                hostname: 'avatars.githubusercontent.com'
+            },
+            {
+                protocol: 'https',
+                hostname: 'cloudflare-ipfs.com'
+            },
+            {
+                protocol: 'https',
+                hostname: 'loremflickr.com'
             }
         ]
     },
--- src/app/components/sessions/main-session.tsx
+++ src/app/components/sessions/main-session.tsx
@@ -13,7 +13,7 @@ export default function MainSession({ session }: Props) {
     return (
         <div className="mx-auto max-w-[790px] p-6">
             <div className="animate-bg-move rounded-3xl bg-gradient-to-r from-indigo-700 via-pink-200 to-indigo-900 bg-400% p-1">
-                <div className={`w-full rounded-3xl p-10 text-left text-white  ${darkBg} ${darkFontColor}`}>
+                <div className={`w-full rounded-3xl p-10 text-left text-white ${darkBg} ${darkFontColor}`}>
                     <h3 className="mb-4 text-5xl font-light sm:text-6xl">{session.name}</h3>
                     <div className="mb-8 flex flex-wrap items-center gap-2">
                         {session.tags.map(({ name, colorClass }, index) => (
