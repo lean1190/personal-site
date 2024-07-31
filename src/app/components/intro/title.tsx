b590543c625bdda71d26dc9b455a9170981a30f7
Lean <leian1306@gmail.com>
2024-07-31 15:50:38 +0200

    intro: change wording of title

--- src/app/components/intro/title.tsx
+++ src/app/components/intro/title.tsx
@@ -1,12 +1,11 @@
-import { highlightText } from '@/lib/strings';
-
 import CtaButton from '../ui/cta-button';
 
 export default function Title() {
     return (
         <section className="relative mx-auto flex max-w-[740px] flex-col items-center gap-8 p-6 pb-16 pt-40 sm:pb-24 sm:pt-32">
-            <h1 className="text-center text-4xl font-bold leading-tight sm:text-6xl sm:leading-tight">
-                <p className="mb-4 sm:mb-0 sm:inline">Get your first job in tech with</p>{' '}{highlightText('CONFIDENCE')}
+            <h1 className="text-center text-5xl font-bold leading-tight sm:text-6xl sm:leading-tight">
+                Get job-ready{' '}
+                <i className="underline decoration-indigo-500 underline-offset-8">FAST</i>
             </h1>
             <p className="max-w-[500px] text-center text-lg font-light sm:text-xl">
                 Leverage your experience to stand out in the market, solidify your knowledge, and build <strong className="font-bold">your path to a successful career</strong>
