6630a09b88fb358e6a9449dca381ff33e924f129
Lean <leian1306@gmail.com>
2024-08-15 14:08:58 +0200

    trust: add links for better SEO

--- src/app/components/trust/companies.tsx
+++ src/app/components/trust/companies.tsx
@@ -1,14 +1,17 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Companies() {
     return (
         <>
             <div className="mb-8 flex flex-col flex-wrap items-center justify-center gap-12 sm:flex-row">
-                <div className="flex items-center gap-2 transition hover:grayscale-0 sm:grayscale">
+                <Link href="https://adplist.org/mentors/leandro-nicolas-vilas" target="_blank" className="flex items-center gap-2 transition hover:grayscale-0 sm:grayscale">
                     <Image src="/logos/toprated.png" width={60} height={69} alt="ADPList 1% mentor" />
                     <Image src="/logos/adplist_logo.svg" width={160} height={41} alt="ADPList logo" />
-                </div>
-                <Image className="hidden grayscale transition hover:grayscale-0 sm:block" src="/logos/hcs_logo.png" width={160} height={75} alt="HCS logo" />
+                </Link>
+                <Link href="https://hamburgcodingschool.com/en/team/lean/" target="_blank">
+                    <Image className="hidden grayscale transition hover:grayscale-0 sm:block" src="/logos/hcs_logo.png" width={160} height={75} alt="HCS logo" />
+                </Link>
                 <Image className="hidden grayscale transition hover:grayscale-0 sm:block" src="/logos/awork_logo.webp" width={160} height={44} alt="Awork logo" />
                 <Image className="hidden grayscale transition hover:grayscale-0 sm:block" src="/logos/fashion_cloud_logo.png" width={160} height={43} alt="Fashion Cloud logo" />
                 <Image className="hidden grayscale transition hover:grayscale-0 sm:block" src="/logos/fluxit_logo.jpg" width={140} height={70} alt="FluxIt logo" />
